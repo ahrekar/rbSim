58b70d69e1dd6b28377f5dd844962b978ba98c79
Karl Ahrendsen <karlahr2010@gmail.com>
2016-01-08 12:21:28 -0600

    Whoo hoo! It compiled. Still in the bare-bones stage, but it's starting to look kind of like what the final version of the program will look like.

--- rbSim.cpp
+++ rbSim.cpp
@@ -2,23 +2,17 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <Particle.h>
-#include <ParticleGenerator.h>
+#include "Particle.h"
+#include "ParticleGenerator.h"
 
 int main(int argc, char* argv[]){
 	//int n = stoi(argv[1]); 
 	int n = 1;
 
-	std::vector<particle> particles;
-	particleGenerator pg = new particleGenerator();
+	std::vector<Particle> particles;
+	ParticleGenerator pg();
 
 	for(int i =0; i < n; i++){
-		numbers.insert(numbers.begin(),1);
-	}
-
-
-	for (int i=0; i < 4; i++){
-		printf("%d\n",numbers[i]);
 	}
 
 	return 0;
