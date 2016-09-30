12be1610f4eb04af8e99f37c134ab0e3063cd2d9
Karl Ahrendsen <karlahr2010@gmail.com>
2016-09-30 15:43:27 -0500

    No errors are presenting themselves at this point. The electric field is now pointing in the appropriate direction and the percent of electrons that make it through the chamber vs. the number density seems to make sense.

--- BeerLambert/beerlambertmc.py
+++ BeerLambert/beerlambertmc.py
@@ -28,7 +28,7 @@ b = 1.2 ** 2 * 2.84304
 # Combines conditions of electron propagation into a single object, Box.
 class Box:
     def __init__(self, rbndensity=0, buffernDensity=10 ** 16, aperture=0.2,
-                 isotropic=True, magnetfield=0, electrfield=0, path3d=False):
+            isotropic=True, magnetfield=0, electrfield=0, path3d=False):
         # The number density of rubidium in the cell.
         self.rbnDensity = rbndensity
         # The number density of bufferGas in the cell
@@ -62,7 +62,7 @@ class Electron:
         # Point of initialization
         self.scattpt = [0, 0, 0]
         # List of scatter points.
-        self.scattlist = [(0, 0, 0)]
+        self.scattlist = []
         # List of scattering direction at each point
         self.directlist = []
         # When recordpath=True, discretized path of electron. See box.rp.
@@ -79,6 +79,9 @@ class Electron:
     # Move electron to next scattering point and calculate new scattering direction. Record scattering point and
     # previous direction in scattlist and directlist respectively.
     def movestep(self):
+        # Record position and direction of particle.
+        self.scattlist.append((self.scattpt[0],self.scattpt[1],self.scattpt[2]))
+        self.directlist.append((self.direct[0],self.direct[1],self.direct[2]))
 
         # First, we need to find out if we have particles in our chamber. We sum all of the number densities.
         totalnDen = self.box.rbnDensity + self.box.buffernDensity
@@ -89,33 +92,32 @@ class Electron:
                 s = 1.1 * (self.box.length + self.arcpsec(self.direct) * self.tblength(self.box.length, self.direct))
             else:
                 s = 1.1 * self.box.length
-        # Calculate a randomized distance between scattering events when number density of species is greater than 0
-        else:
+        else:# Calculate a randomized distance between scattering events when number density of species is greater than 0
             s = self.randomstepsize()
 
         if self.box.magnet != 0 or self.box.electr != 0:
             # Find time needed to travel given step size with nonzero magnetic and/or electric field
             t = self.tpath(s, self.direct)
+            #print("Time:{}".format(t))
             # Interpolate path between scatter points of electron within magnetic field
             if self.box.rp and self.box.magnet != 0:
                 for i in linspace(0, t, 500):
                     self.path.append(
-                        (self.scattpt[0] + self.x(i, self.direct), self.scattpt[1] + self.y(i, self.direct),
-                         self.scattpt[2] + self.z(i, self.direct)))
-            # Find new scatter points and update electron velocity at time of scattering.
+                            (self.scattpt[0] + self.x(i, self.direct), self.scattpt[1] + self.y(i, self.direct),
+                                self.scattpt[2] + self.z(i, self.direct)))
+                            # Find new scatter points and update electron velocity at time of scattering.
             self.scattpt[0] += self.x(t, self.direct)
             self.scattpt[1] += self.y(t, self.direct)
             self.scattpt[2] += self.z(t, self.direct)
+            #print("Position:({},{},{})".format(self.scattpt[0],self.scattpt[1],self.scattpt[2]))
             self.speed = self.newvel(t, self.direct)
         else:
             # Find new scatter points when magnetic and electric fields are zero. Path of electron is linear.
             self.scattpt[0] += self.direct[0] * s
             self.scattpt[1] += self.direct[1] * s
             self.scattpt[2] += self.direct[2] * s
-        # Record scatter point and previous scattering direction.
-        self.scattlist.append((self.scattpt[0], self.scattpt[1], self.scattpt[2]))
-        self.directlist.append((self.direct[0], self.direct[1], self.direct[2]))
 
+    def scatter(self):
         # Find new random scattering direction. Phi-azimuth angle [0, 2pi). Theta-altitude. [0, pi).
         phi = uniform(0, 2 * pi)
         theta = self.randomtheta()
@@ -132,9 +134,9 @@ class Electron:
         # direction of electron is not parallel to z axis.
         else:
             self.direct[0] = (sin(theta) * cos(phi) * ux * uz - sin(theta) * sin(phi) *
-                              uy) / sqrt(1 - uz ** 2) + ux * cos(theta)
+                    uy) / sqrt(1 - uz ** 2) + ux * cos(theta)
             self.direct[1] = (sin(theta) * cos(phi) * uy * uz + sin(theta) * sin(phi) *
-                              ux) / sqrt(1 - uz ** 2) + uy * cos(theta)
+                    ux) / sqrt(1 - uz ** 2) + uy * cos(theta)
             self.direct[2] = -sin(theta) * cos(phi) * sqrt(1 - uz ** 2) + uz * cos(theta)
 
         xi = uniform(0, 1)
@@ -152,18 +154,16 @@ class Electron:
                 # Component parallel to z axis of Nitrogen velocity after scattering event. Used to determine
                 # post scattering velocity of electron with components vx, vy, and vz
                 vz2 = (2 * self.speed * d[2] + 2 * tan(theta) * (cos(phi) * self.speed * d[0] +
-                                      sin(phi) * self.speed * d[1])) / \
-                      ((1 + tan(theta) ** 2) * (1 + (n2mass / emass)))
+                    sin(phi) * self.speed * d[1])) / \
+                            ((1 + tan(theta) ** 2) * (1 + (n2mass / emass)))
                 vx = self.speed * d[0] - tan(theta) * cos(phi) * (n2mass / emass) * vz2
                 vy = self.speed * d[1] - tan(theta) * sin(phi) * (n2mass / emass) * vz2
                 vz = self.speed * d[2] - (n2mass / emass) * vz2
                 # Find magnitude of electron velocity post scattering.
                 self.speed = sqrt(vx ** 2 + vy ** 2 + vz ** 2)
-            # Terminate electron. Total inelastic scattering when energy of electron between a and b.
-            else:
-                self.speed = 0
-        # Electron collides with Rubidium atom. Switch spin polarization if opposites.
-        else:
+            else:# Terminate electron. Total inelastic scattering when energy of electron between a and b.
+                self.speed = 0.00001
+        else:# Electron collides with Rubidium atom. Switch spin polarization if opposites.
             if xi < 0.5 and self.color == 'r':
                 self.color = 'b'
             elif xi > 0.5 and self.color == 'b':
@@ -175,6 +175,12 @@ class Electron:
         xi = -uniform(-1, 0)
         # Alpha may be considered as the cross section of all attenuating particles within a cubic cm.
         alpha = self.rbCrossSection() * self.box.rbnDensity + self.bufferCrossSection() * self.box.buffernDensity
+        #print("bufferCrossSection: {}".format(self.bufferCrossSection()))
+        #print("bufferNDensity: {}".format(self.box.buffernDensity))
+        #print("rbCrossSection: {}".format(self.rbCrossSection()))
+        #print("rbNDensity: {}".format(self.box.rbnDensity))
+        #print("Alpha: {}".format(alpha))
+        #print("Electron Vel: {}".format(self.speed))
         return -log(xi) / alpha
 
     # Randomly sample theta/altitude angle of scattering direction. Derived via Inverse transform sampling.
@@ -183,13 +189,14 @@ class Electron:
         # Isotropic scattering
         if self.box.isotrop:
             return arccos(1 - 2 * xi)
-        # Anisotropic/Forward scattering. Probability density function proportional to 1+cos(theta)
-        else:
+        else:# Anisotropic/Forward scattering. Probability density function proportional to 1+cos(theta)
             return arccos((-2 + (16 - 16 * xi) ** 0.5) / 2)
 
     # Determine scattering cross section of Rubidium atom- dependent on electron velocity (cm/s)
     def rbCrossSection(self):
+        #print("Note: The Rubidium cross section has been compromised to diagnose the self.speed = nan issue")
         return exp(-self.speed / 10 ** 8) * 10 ** 14
+        #return exp(-10**8 / 10 ** 8) * 10 ** 14
 
     # Determine scattering cross section of Nitrogen molecule. Dependent on energy of electron.
     def bufferCrossSection(self):
@@ -225,6 +232,10 @@ class Electron:
     # Calculate z location of electron in presence of magnetic and electric field after time t given scattering
     # direction
     def z(self, t, d):
+        #print("Position: {}".format(self.scattpt[2]))
+        #print("Electric Field Contribution:{}".format(0.5 * cmr * self.box.electr * t ** 2))
+        #print("Velocity Contribution:{}".format(d[2] * self.speed * t))
+        #print("Time:{}".format(t))
         return 0.5 * cmr * self.box.electr * t ** 2 + d[2] * self.speed * t
 
     # Calculate arc length in xy plane traveled per second in magnetic field.
@@ -233,26 +244,29 @@ class Electron:
 
     # Calculate time required to travel given arc length and initial scattering direction.
     def tpath(self, s, d):
-        if self.box.electr != 0:
+        if self.box.magnet != 0:
             if (d[2] * self.speed) ** 2 + 2 * cmr * self.box.electr * s * sign(d[2]) < 0:
                 return s / self.arcpsec(d)
             else:
                 l = self.arcpsec(d)
                 return (-(d[2] * self.speed + sign(d[2]) * l) + sign(d[2]) * sqrt(
                     (d[2] * self.speed + sign(d[2]) * l) ** 2 + 2 * cmr * self.box.electr * s * sign(d[2]))) / (
-                           cmr * self.box.electr)
+                            cmr * self.box.electr)
         else:
             return (s * sign(d[2])) / (d[2] * self.speed)
 
     # Calculate time required to travel given length along z axis (z) given initial scattering direction (d).
     def tblength(self, z, d):
+        #print("Position: {}".format(self.scattpt[2]))
+        #print("Electric Field Contribution:{}".format(2 * cmr * self.box.electr * z))
+        #print("Velocity Contribution:{}".format((d[2] * self.speed)**2))
         if self.box.electr != 0:
             value1 =(-d[2] * self.speed + sqrt(
                 (d[2] * self.speed) ** 2 + 2 * cmr * self.box.electr * z)) / (
-                       cmr * self.box.electr)
+                        cmr * self.box.electr)
             value2 =(-d[2] * self.speed - sqrt(
-                (d[2] * self.speed) ** 2 + 2 * cmr * self.box.electr * z)) / (
-                       cmr * self.box.electr)
+                    (d[2] * self.speed) ** 2 + 2 * cmr * self.box.electr * z)) / (
+                            cmr * self.box.electr)
             if value1 > 0:
                 return value1
             else:
@@ -262,16 +276,18 @@ class Electron:
 
     # Update velocity after travel between scattering events due to electric/magnetic field.
     def newvel(self, t, d):
+        #print("t: {}".format(t))
+        #print("d: {}".format(d))
         return sqrt((self.speed * (-d[1] * sin(cmr * self.box.magnet * t) +
-                                 d[0] * cos(cmr * self.box.magnet * t))) ** 2 +
-                    (self.speed * (d[0] * sin(cmr * self.box.magnet * t) +
-                                 d[1] * cos(cmr * self.box.magnet * t))) ** 2 +
-                    (cmr * self.box.electr * t + d[2]) ** 2)
+            d[0] * cos(cmr * self.box.magnet * t))) ** 2 +
+            (self.speed * (d[0] * sin(cmr * self.box.magnet * t) +
+                d[1] * cos(cmr * self.box.magnet * t))) ** 2 +
+            (cmr * self.box.electr * t + d[2] * self.speed) ** 2)
 
-    # Determine if electron is within the attenuating chamber. If outside, delete scattlist and directlist and
+        # Determine if electron is within the attenuating chamber. If outside, delete scattlist and directlist and
     # terminate.
     def inbox(self):
-        d = self.directlist[-1]
+        d = self.direct
         r = sqrt((self.scattpt[0]) ** 2 + (self.scattpt[1]) ** 2)
         if self.scattpt[2] < 0:
             self.scattlist = [(0, 0, 0,)]
@@ -279,11 +295,7 @@ class Electron:
             self.alive = False
         elif r > self.box.radius:
             self.scattlist = [(0, 0, 0)]
-            self.directlist = [(0, 0, 0,)]
-            self.alive = False
-        elif self.box.magnet != 0 and (self.speed * sqrt(d[0] + d[1])) / (-cmr * self.box.magnet) + r > self.box.radius:
-            self.scattlist = [(0, 0, 0)]
-            self.directlist = [(0, 0, 0,)]
+            self.directlist = [(0, 0, 0)]
             self.alive = False
         elif self.scattpt[2] > self.box.length:
             self.alive = False
@@ -297,21 +309,21 @@ class Electron:
                 # box along trajectory. Compare distance from (0, 0, box.length). Return true if less than aperture
                 # radius.
                 if self.box.electr != 0 or self.box.magnet != 0:
-                    t = self.tblength(self.scattlist[-2][2] - self.box.length, self.directlist[-1])
-                    r = sqrt((self.x(t, self.directlist[-1]) + self.scattlist[-2][0]) ** 2 +
-                             (self.y(t, self.directlist[-1]) + self.scattlist[-2][1]) ** 2)
+                    t = self.tblength(self.scattlist[-1][2] - self.box.length, self.direct)
+                    r = sqrt((self.x(t, self.direct) + self.scattlist[-1][0]) ** 2 +
+                            (self.y(t, self.direct) + self.scattlist[-1][1]) ** 2)
                     if r < self.box.aper:
                         return True
                 # When magnetic and electric fields are zero, calculate location of electron at the length of the box
-                # along trajectory. Compare distance from (0, 0, box.length). Return true is less than aperture radius.
+                # along trajectory. Compare distance from (0, 0, box.length). Return true if less than aperture radius.
                 else:
-                    r0 = self.scattlist[-2]
-                    r1 = self.scattlist[-1]
+                    r0 = self.scattlist[-1]
+                    r1 = self.scattpt
                     t = (self.box.length - r0[2]) / (r1[2] - r0[2])
                     r = sqrt((r0[0] + t * (r1[0] - r0[0])) ** 2 + (r0[1] + t * (r1[1] - r0[1])) ** 2)
                     if r < self.box.aper:
                         return True
-                return False
+                    return False
             except IndexError:
                 return False
         else:
--- ExcitationFn.py
+++ ExcitationFn.py
@@ -1,37 +1,60 @@
 from BeerLambert.beerlambertmc import *
 
-
 # Plot Radius of Aperture vs. -log(transmission) / (b.sigt * b.length * b.nden). Depicts deviation of from Beer-Lambert
 # Law for larger apertures
-def excitationFn(count=50000, isotrop=False, electr=100, magnet=0):
+def excitationFn(count=50000, buffernDensity=1e10, isotrop=False, sweepingPotential=3, magnet=0):
+    # The length of the chamber in m
+    boxLength=.03
     # Set conditions of attenuating chamber
-    b = Box(buffernDensity=0, isotropic=isotrop, electrfield=electr, magnetfield=magnet)
+    b = Box(buffernDensity=buffernDensity, isotropic=isotrop, electrfield=-sweepingPotential/boxLength, magnetfield=magnet)
     electronsThrough=[]
-    for i in range(1,count+1):
+    for i in range(count):
         p=Electron(b)
+        #print("New Electron!")
         steps=0
-        while p.alive and steps < 1000000:
+        while p.alive:
             p.movestep()
+            #print("{} {} {}".format(p.scattpt[0],p.scattpt[1],p.scattpt[2]))
+            if p.thruaper:
+                """
+                if (i+1)%75 == 0:
+                    print('*')
+                else:
+                    print('*',end="",flush=True)
+                """
+                electronsThrough.append(p)
+            else:
+                """
+                if (i+1)%75 == 0:
+                    print('.')
+                else:
+                    print('.',end="",flush=True)
+                """
             p.inbox()
-            #print(p.speed)
+            p.scatter()
             steps+=1
-        if p.thruaper:
-            if i%100 == 0:
-                print('*')
-            else:
-                print('*',end="",flush=True)
-            electronsThrough.append(p)
-        else:
-            if i%100 == 0:
-                print('.')
-            else:
-                print('.',end="",flush=True)
 
-    print("# Electrons through:{0}".format(len(electronsThrough)))
+    print("% Electrons through: {0:.3}".format(len(electronsThrough)/count))
     averageEnergy=0
     for elec in electronsThrough:
         averageEnergy+=elec.energyev()
+    return len(electronsThrough)/count
+"""
     if averageEnergy:
         print(averageEnergy/len(electronsThrough))
+    """
+def DensityVSPercentThrough():
+    file = open('test.txt','w')
 
-excitationFn(count=30000,electr=100,magnet=0)
+    file.write("Density\tPercentThrough\n")
+    for i in linspace(8,17,50):
+        file.write('{:.3e}'.format(10**i))
+        file.write('\t')
+        file.write('{:.3e}'.format(excitationFn(count=30000,sweepingPotential=3,magnet=0,buffernDensity=10**i)))
+        file.write('\n')
+
+    file.close()
+
+DensityVSPercentThrough()
+#excitationFn(count=100,sweepingPotential=3,magnet=0,buffernDensity=1e15)
+
